06a546a843023915fd48e0e2dc0d66670444f04d
Cotton Top <mtmcgr@gmail.com>
2020-01-27 20:19:56 -0600

    file structure

---
+++ directory-react/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Filter from './components/Filter';
+import Sort from './components/Sort';
+import Table from './components/Table';
+import Row from './components/Row';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+         Employee Directory
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
