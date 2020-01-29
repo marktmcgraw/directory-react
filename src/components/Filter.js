32e17c5c239873a8fe700fc9fc0205065ce68b46
Cotton Top <mtmcgr@gmail.com>
2020-01-29 08:26:12 -0600

    sort and filter components

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-
-export default function Filter(props){
-
+export default function Filter(props) {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <input
+            type="radio"
+            value="manager"
+            onChange={({ target }) => props.onChange(target.checked)}
+          />
+          <label className="m-1"> Show only manager</label>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-
-export default function Sort(props){
-
+export default function Sort(props) {
+  return (
+    <div className="container-fluid">
+      <div className="row bg-lightgray text-black p-1">
+        <div className="col-4 mb-1 mx-1">Sort by </div>
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <select
+            className="form-control "
+            onChange={({ target }) => props.onChange(target.value)}
+          >
+            <option> Department </option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 }
